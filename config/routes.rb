aad6639e72df0a594015ae4fe450e4390a4cf363
Sam Sun <sam912358@gmail.com>
2014-07-25 17:03:24 +0800

    Add follow and followed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 SampleApp::Application.routes.draw do
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  
+  resources :relationships, only: [:create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
   root 'static_pages#home'
